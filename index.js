da7cabef987eb04a1a9b3e8252654cb31ab09d3f
Ted Wu <iswujixiang@gmail.com>
2017-12-10 01:15:52 +0800

    refactor: use closure to save the composed middleware

--- index.js
+++ index.js
@@ -10,49 +10,53 @@ const compose   = require('koa-compose');
 
 const app       = new Koa;
 
-const k2e       = middlewares => (req, res, next) => {
-
-    const ctx   = app.createContext(req, res);
-
+const k2e       = middlewares => {
     if (!Array.isArray(middlewares)) {
         middlewares = [middlewares];
     }
 
-    compose(middlewares)(ctx)
+    const middleware = compose(middlewares);
 
-        .then(() => {
-            // allow bypassing koa
-            /* istanbul ignore if */
-            if (ctx.respond === false) {
-                return next();
-            }
+    return (req, res, next) => {
 
-            /* istanbul ignore if */
-            if (!ctx.writable) {
-                return next();
-            }
+        const ctx   = app.createContext(req, res);
 
-            const {res, body} = ctx;
-            if (null != body) {
-                if (Buffer.isBuffer(body)) {
-                    return res.send(body);
-                }
-                if (typeof body === 'string') {
-                    return res.send(body);
-                }
-                if (body instanceof Stream) {
-                    return body.pipe(res);
+        middleware(ctx)
+
+            .then(() => {
+                // allow bypassing koa
+                /* istanbul ignore if */
+                if (ctx.respond === false) {
+                    return next();
                 }
 
-                // body: json
-                return res.json(body);
-            }
+                /* istanbul ignore if */
+                if (!ctx.writable) {
+                    return next();
+                }
 
-            next();
+                const {res, body} = ctx;
+                if (null != body) {
+                    if (Buffer.isBuffer(body)) {
+                        return res.send(body);
+                    }
+                    if (typeof body === 'string') {
+                        return res.send(body);
+                    }
+                    if (body instanceof Stream) {
+                        return body.pipe(res);
+                    }
 
-        })
+                    // body: json
+                    return res.json(body);
+                }
 
-        .catch(next);
+                next();
+
+            })
+
+            .catch(next);
+    };
 };
 
 module.exports  = k2e;
