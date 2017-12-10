37a6df6a8e1c9bc141b4d16cbe4b6a0290b4ee52
Ted Wu <iswujixiang@gmail.com>
2017-12-11 02:25:13 +0800

    feat: express to koa

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Stream    = require('stream');
 
 const Koa       = require('koa');
 const compose   = require('koa-compose');
+const express   = require('express');
+const {init}    = require('express/lib/middleware/init');
 const debug     = require('debug')('koa-to-express');
 
 const app       = new Koa;
@@ -18,7 +20,7 @@ const k2e       = middlewares => {
 
     const middleware = compose(middlewares);
 
-    debug(`converted middlewares: ${middlewares.map(({name}) => name || '[anonymous]').join()}`);
+    debug(`converted koa middlewares: ${middlewares.map(({name}) => name || '[anonymous]').join()}`);
 
     return (req, res, next) => {
 
@@ -62,4 +64,53 @@ const k2e       = middlewares => {
     };
 };
 
+const expressApp = express();
+const {request, reponse} = expressApp;
+
+const e2k       = middlewares => {
+    if (Array.isArray(middlewares)) {
+        return compose(middlewares.map(e2k));
+    }
+
+    if (middlewares.length === 4) {
+        throw new Error('e2k do not support error-handler now');
+    }
+
+    debug(`converted express middleware: ${middlewares.name || '[anonymous]'}`);
+
+    return (ctx, next) => {
+
+        const {req, res, query} = ctx;
+
+        if (!req.query) req.query = query; // express's query middleware
+
+        if (!(req.__proto__ === request && res.__proto__ === reponse)) {
+            init(expressApp)(req, res, function () {
+                // do nothing
+            });
+        }
+        return new Promise((resolve, reject) => {
+            try {
+                res
+                    .status(200)
+                    .on('finish', resolve);
+                middlewares(req, res, err => {
+                    if (!err) {
+                        return resolve('next');
+                    }
+                    reject(err);
+                });
+            } catch (err) {
+                reject(err);
+            }
+        }).then(_next => {
+            if (_next) {
+                res.status(404);
+                return next();
+            }
+        });
+    };
+};
+
 module.exports  = k2e;
+module.exports.expressToKoa = e2k;
